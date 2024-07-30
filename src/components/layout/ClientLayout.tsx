8ec7d78c73f24868dab1f96747cd2e20cf31dcab
ArnaldoLima12 <arnaldolima588@gmail.com>
2024-07-30 10:18:31 -0300

    v2

--- src/components/layout/ClientLayout.tsx
+++ src/components/layout/ClientLayout.tsx
@@ -6,43 +6,45 @@ import 'primeicons/primeicons.css';
 import "@/styles/globals.css";
 
 export default function ClientLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  
+
   const [theme, setTheme] = useState<string>('lara-dark-blue'); // Tema padrão
+  const [load, setLoadding] = useState<boolean>(true);
 
   useEffect(() => {
-  
+
     const getInitialTheme = () => {
-      if (typeof window !== "undefined") { // Verifica se está no ambiente do navegador
-        
-        const userTheme = localStorage.getItem('theme');
-        
-        if (userTheme) {
-          const theme = JSON.parse(userTheme);
-          return theme.css;
-        }
+
+      const userTheme = localStorage?.getItem('theme');
+
+      if (userTheme) {
+        const theme = JSON.parse(userTheme);
+        setTheme(theme.css)
       }
-      return 'lara-dark-blue'; // Tema padrão
+
+      setLoadding(false);
     };
 
-    const initialTheme = getInitialTheme();
-    setTheme(initialTheme);
+    getInitialTheme();
+
   }, []);
 
   return (
-    <html lang="pt-br">
-      <head>
-        <link
-          id="theme-css"
-          rel="stylesheet"
-          href={`/themes/${theme}/theme.css`}
-        />
-      </head>
-      <body>
-        <Container>
-          <Header />
-          {children}
-        </Container>
-      </body>
-    </html>
+        <html lang="pt-br">
+          <head>
+            <link
+              id="theme-css"
+              rel="stylesheet"
+              href={`/themes/${theme}/theme.css`}
+            />
+          </head>
+          <body>
+          {load !== true && (
+            <Container>
+              <Header />
+              {children}
+            </Container>
+          )}
+          </body>
+        </html>
   );
 }
