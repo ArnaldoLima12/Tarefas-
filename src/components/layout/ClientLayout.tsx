ae2e3a414caa7f117486fb61105a9c1d9570b602
ArnaldoLima12 <arnaldolima588@gmail.com>
2024-07-29 22:01:25 -0300

    v1

--- src/components/layout/ClientLayout.tsx
+++ src/components/layout/ClientLayout.tsx
@@ -1,27 +1,29 @@
 'use client'
+import { useEffect, useState } from "react";
 import Container from "@/components/layout/Container";
 import Header from "@/components/layout/Header";
 import 'primeicons/primeicons.css';
-import "@/styles/globals.css"
-
-
+import "@/styles/globals.css";
 
 export default function ClientLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const [theme, setTheme] = useState<string>('lara-dark-blue'); // Tema padrão
 
-  // Função para verificar o tema no localStorage
-  const getInitialTheme = () => {
+  useEffect(() => {
+    // Função para verificar o tema no localStorage
+    const getInitialTheme = () => {
+      if (typeof window !== "undefined") { // Verifica se está no ambiente do navegador
+        const userTheme = localStorage.getItem('theme');
+        if (userTheme) {
+          const theme = JSON.parse(userTheme);
+          return theme.css;
+        }
+      }
+      return 'lara-dark-blue'; // Tema padrão
+    };
 
-    const userTheme = localStorage.getItem('theme');
-
-    if (userTheme) {
-      const theme = JSON.parse(userTheme);
-      return theme.css;
-    }
-
-    return 'lara-dark-blue'; // Tema padrão
-  };
-
-  const initialTheme = getInitialTheme();
+    const initialTheme = getInitialTheme();
+    setTheme(initialTheme);
+  }, []);
 
   return (
     <html lang="pt-br">
@@ -29,7 +31,7 @@ export default function ClientLayout({ children }: Readonly<{ children: React.Re
         <link
           id="theme-css"
           rel="stylesheet"
-          href={`/themes/${initialTheme}/theme.css`}
+          href={`/themes/${theme}/theme.css`}
         />
       </head>
       <body>
